5577ee44e71033556bfd4f74aba1e674a6d40b5b
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2021-06-10 20:45:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,13 +17,13 @@ function setup(){
 createCanvas(800,600);
 //create 4 different surfaces
 Surface1 = createSprite(100,590,180,30);
-Surface1.Colour = "green";
+Surface1.shapeColor = "green";
 Surface2 = createSprite(300,590,180,30);
-Surface2.Colour = "red";
+Surface2.shapeColor = "red";
 Surface3 = createSprite(500,590,180,30);
-Surface3.Colour = "blue";
+Surface3.shapeColor = "blue";
 Surface4 = createSprite(700,590,180,30);
-Surface4.shapeColour = rgb(200,147,32);
+Surface4.shapeColor = "yellow";
 
     //create box sprite and give velocity
 Box = createSprite(600,300,40,40);
@@ -46,17 +46,21 @@ drawSprites();
     //add condition to check if box touching surface and make it box
     if(Box.isTouching(Surface1)){
         Box.bounceOff(Surface1);
+        Box.shapeColor = "green";
     }
     if(Box.isTouching(Surface2)){
+        Box.shapeColour = "red";
         Box.velocityX = 0;
         Box.velocityY = 0;
-        Box.shapeColour = rgb(255,128,0);
+        text("Game Over",200,200);
     }
     if(Box.isTouching(Surface3)){
         Box.bounceOff(Surface3);
+        Box.shapeColor = "blue";
     }
     if(Box.isTouching(Surface4)){
         Box.bounceOff(Surface4);
+        Box.shapeColor = "yellow";
     }
     
 }
